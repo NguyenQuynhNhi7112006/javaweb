b55f1b4a4f28ce36be917e83abf1f306528a5b1b
NguyenQuynhNhi7112006 <nhi7112006@gmail.com>
2026-08-19 20:08:54 +0700

    Delete an-paper/an-paper directory

--- an-paper/an-paper/src/main/java/an_paper/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package an_paper.repository;
-
-import an_paper.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByProductIdOrderByCreatedAtDesc(Long productId);
-}
